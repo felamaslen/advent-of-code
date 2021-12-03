b87d7ba94d9253dc9046601be07c893d47e1a7fa
Fela Maslen <felamaslen@gmail.com>
2021-12-03 17:52:05 +0000

    day 1: task 2

--- 2021/day1/src/Main.hs
+++ 2021/day1/src/Main.hs
@@ -1,5 +1,7 @@
 import Text.Printf
 
+inputFile = "./input.txt"
+
 stringToInt :: String -> Int
 stringToInt s = read s :: Int
 
@@ -12,11 +14,31 @@ countIncreases [] = 0
 countIncreases [value] = 0
 countIncreases values = countIncrease ((values !! 1) > head values) + countIncreases (drop 1 values)
 
+sumWindow :: Int -> [Int] -> Int
+sumWindow n values
+  | length values >= n = sum (take n values)
+  | otherwise = 0
+
+countWindowedIncreases :: Int -> [Int] -> Int
+countWindowedIncreases n values
+  | length values >= n =
+    countIncrease (sumWindow n (drop 1 values) > sumWindow n values) + countWindowedIncreases n (drop 1 values)
+  | otherwise = 0
+
+task1 :: [Int] -> IO ()
+task1 values = do
+  let numIncreases = countIncreases values
+  printf "Task 1: numIncreases=%d\n" numIncreases
+
+task2 :: [Int] -> IO ()
+task2 values = do
+  let numWindowedIncreases = countWindowedIncreases 3 values
+  printf "Task 2: numWindowedIncreases=%d\n" numWindowedIncreases
+
 main = do
-  content <- readFile "./input.txt"
+  content <- readFile inputFile
   let chars = lines content
   let values = map stringToInt chars
 
-  let numIncreases = countIncreases values
-
-  printf "Task 1: numIncreases=%d\n" numIncreases
+  task1 values
+  task2 values
