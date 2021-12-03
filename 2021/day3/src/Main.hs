c612d04600720192d3f23aabfe0fc121cc86f203
Fela Maslen <felamaslen@gmail.com>
2021-12-03 19:43:55 +0000

    day 3 task 1

---
+++ 2021/day3/src/Main.hs
@@ -0,0 +1,78 @@
+import Data.Bits
+import Text.Printf
+
+inputFile = "./input.txt"
+
+boolToInt :: Bool -> Int
+boolToInt False = 0
+boolToInt True = 1
+
+mapBinaryCharLine :: String -> Int
+mapBinaryCharLine [] = 0
+mapBinaryCharLine (x:xs) = (read [x] :: Int) * (2 ^ length xs) + mapBinaryCharLine xs
+
+numOneNthBits :: [Int] -> Int -> Int
+numOneNthBits xs index = foldr
+  (\ x -> (+) (boolToInt (testBit x index))) 0 xs
+-- numOneNthBits [] index = 0
+-- numOneNthBits (x:xs) index = boolToInt (testBit x index) + numOneNthBits xs index
+
+one = 1
+zero = 0
+neither = -1
+
+flipMostCommon :: Int -> Int
+flipMostCommon v
+  | v == one = zero
+  | v == zero = one
+  | otherwise = v
+
+mostCommon :: Int -> Int -> Int
+mostCommon numValues numOnes
+  | numOnes > numValues - numOnes = one
+  | numOnes < numValues - numOnes = zero
+  | otherwise = neither
+
+mostCommonNthBit :: [Int] -> Int -> Int
+mostCommonNthBit values index = mostCommon
+  (length values)
+  (numOneNthBits values index)
+
+leastCommonNthBit :: [Int] -> Int -> Int
+leastCommonNthBit values index = flipMostCommon (mostCommonNthBit values index)
+
+setOrClearBit :: Int -> Int -> Int -> Int
+setOrClearBit value i oneOrZero
+  | oneOrZero == 0 = clearBit value i
+  | oneOrZero == 1 = setBit value i
+  | otherwise = value
+
+constructNthBitLoop :: ([Int] -> Int -> Int) -> [Int] -> Int -> Int -> Int
+constructNthBitLoop getNthBit values index result
+  | index == 0 = setOrClearBit result 0 (getNthBit values 0)
+  | index > 0 = setOrClearBit (
+    constructNthBitLoop getNthBit values (index - 1) result
+  ) index (getNthBit values index)
+
+constructNthBitValue :: ([Int] -> Int -> Int) -> [Int] -> Int -> Int
+constructNthBitValue getNthBit values numBits = constructNthBitLoop getNthBit values (numBits - 1) 0
+
+constructMostCommon = constructNthBitValue mostCommonNthBit
+constructLeastCommon = constructNthBitValue leastCommonNthBit
+
+task1 :: [Int] -> Int -> IO ()
+task1 values numBits = do
+  let gamma = constructMostCommon values numBits
+  let epsilon = constructLeastCommon values numBits
+
+  let powerConsumption = gamma * epsilon
+
+  printf "Task 1: gamma=%d, epsilon=%d, powerConsumption=%d\n" gamma epsilon powerConsumption
+
+main = do
+  content <- readFile inputFile
+  let chars = lines content
+  let numBits = length (head chars)
+  let values = map mapBinaryCharLine chars
+
+  task1 values numBits
