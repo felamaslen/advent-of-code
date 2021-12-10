82bd40b441930ea1daa6e8f19ba451277175c102
Fela Maslen <felamaslen@gmail.com>
2021-12-10 20:22:56 +0000

    day 10 task 2

--- 2021/day10/src/Main.hs
+++ 2021/day10/src/Main.hs
@@ -1,3 +1,4 @@
+import Data.List (sort)
 import Text.Printf
 
 inputFile = "./input.txt"
@@ -47,13 +48,62 @@ getCorruptCharacterScore = maybe 0 getBracketScore
 getCorruptScore :: [String] -> Int
 getCorruptScore chars = sum (map (getCorruptCharacterScore . getIncorrectClosingCharacter) chars)
 
+lineIsNotCorrupt :: String -> Bool
+lineIsNotCorrupt line = case getIncorrectClosingCharacter line of
+                          Just incorrect -> False
+                          Nothing -> True
+
+removeCorruptLines :: [String] -> [String]
+removeCorruptLines = filter lineIsNotCorrupt
+
+completeLineLoop :: String -> ([Char], String) -> String
+completeLineLoop [] ([], result) = result
+completeLineLoop [] (s:stack, result) =
+  completeLineLoop [] (stack, result ++ [getOpenBracketCloseType s brackets])
+completeLineLoop (char:line) (stack, result)
+  | isSomeOpenBracket char = completeLineLoop line (char : stack, result)
+  | charIsCloseBracket char (head stack) = completeLineLoop line (drop 1 stack, result)
+  | otherwise = error "Corrupt line"
+
+getLineCompletion :: String -> String
+getLineCompletion line = completeLineLoop line ([], "")
+
+getCompletionBracketScore :: Char -> Int
+getCompletionBracketScore c
+  | c == ')' = 1
+  | c == ']' = 2
+  | c == '}' = 3
+  | c == '>' = 4
+
+scoreLineCompletionLoop :: String -> Int -> Int
+scoreLineCompletionLoop rest score =
+  foldl (\ score c -> (score * 5) + getCompletionBracketScore c) score rest
+
+scoreLineCompletion :: String -> Int
+scoreLineCompletion line = scoreLineCompletionLoop line 0
+
+scoreCompletedLine :: String -> Int
+scoreCompletedLine line = scoreLineCompletion (getLineCompletion line)
+
+median :: [Int] -> Int
+median as = sort as !! (length as `div` 2)
+
+scoreAllCompletedLines :: [String] -> Int
+scoreAllCompletedLines chars = median (map scoreCompletedLine (removeCorruptLines chars))
+
 task1 :: [String] -> IO ()
 task1 chars = do
   let result = getCorruptScore chars
   printf "Task 1: score=%d\n" result
+
+task2 :: [String] -> IO ()
+task2 chars = do
+  let result = scoreAllCompletedLines chars
+  printf "Task 2: score=%d\n" result
 
 main = do
   content <- readFile inputFile
   let chars = lines content
 
   task1 chars
+  task2 chars
